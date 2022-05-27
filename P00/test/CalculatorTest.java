e8162fc542f83e93666afbe23b8045e54b86f90c
21024102 <21024102@phoebe>
2022-05-27 11:20:46 +0800

    first push to P00 repository in GitHub

---
+++ P00/test/CalculatorTest.java
@@ -0,0 +1,77 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void testAdd() {
+		//fail("Not yet implemented");
+		int a = 1234;
+		int b = 8765;
+		
+		Calculator cal = new Calculator();
+		int actual = cal.add(a, b);
+		
+		int expected = 9999;
+		assertEquals (expected, actual);
+	}
+	
+	@Test
+	public final void testSubtract() {
+		int a = 9876;
+		int b = 4321;
+		
+		Calculator cal = new Calculator();
+		int actual = cal.subtract(a, b);
+		
+		int expected = 5555;
+		assertEquals(actual, expected);
+	}
+	
+	@Test
+	public final void testMultiply() {
+		int a = 1234;
+		int b = 4321;
+		
+		Calculator cal = new Calculator();
+		int actual = cal.multiply(a, b);
+		
+		int expected = 5332114;
+		assertEquals(actual, expected);
+	}
+	
+	@Test
+	public final void testDivide() 
+	{
+		int a = 5000;
+		int b = 1000;
+		
+		Calculator cal = new Calculator();
+		int actual = cal.divide(a, b);
+		
+		int expected = 5;
+		assertEquals(actual, expected);
+	}
+
+}
